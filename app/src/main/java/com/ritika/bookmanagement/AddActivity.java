02b59a70bddd18e59090712273a682d7f05019f4
RitikaAgarwal13 <ritikaagarwal760@gmail.com>
2022-03-26 19:46:24 +0530

    Initial commit

---
+++ app/src/main/java/com/ritika/bookmanagement/AddActivity.java
@@ -0,0 +1,31 @@
+package com.ritika.bookmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+    EditText title_input, author_input, pages_input;
+    Button ad_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_input);
+        ad_button = findViewById(R.id.ad_button);
+        ad_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim(),
+                        pages_input.getText().toString().trim());
+            }
+        });
+    }
+}
